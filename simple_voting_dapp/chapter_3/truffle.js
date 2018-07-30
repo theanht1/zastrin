1d6806e219dfad54580af54e9af2bc42f2853b6a
Anh Mai <theanht1k20@gmail.com>
2018-07-30 09:59:09 +0700

    Configuration for ropsten network deploy

--- simple_voting_dapp/chapter_3/truffle.js
+++ simple_voting_dapp/chapter_3/truffle.js
@@ -1,5 +1,8 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+require('babel-register');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = 'debate vanish tool stem acoustic tank move bird tattoo latin license chuckle';
 
 module.exports = {
   networks: {
@@ -7,6 +10,13 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*' // Match any network id
-    }
-  }
-}
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/7c5a6bb200b04aa0961af87d796358d5');
+      },
+      network_id: 3,
+      gas: 4700000,
+    },
+  },
+};
