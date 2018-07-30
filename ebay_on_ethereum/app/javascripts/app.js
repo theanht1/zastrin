bad6ca33a8856631bb2a11ba9a2bffc897106d08
Anh Mai <theanht1k20@gmail.com>
2018-07-30 17:52:43 +0700

    Use ipfs for product's image and description

--- ebay_on_ethereum/app/javascripts/app.js
+++ ebay_on_ethereum/app/javascripts/app.js
@@ -4,12 +4,18 @@ import '../stylesheets/app.css';
 // Import libraries we need.
 import { default as Web3 } from 'web3';
 import { default as contract } from 'truffle-contract';
+import ipfsAPI from 'ipfs-api';
 
 // Import our contract artifacts and turn them into usable abstractions.
 import ecommerce_store_artifacts from '../../build/contracts/EcommerceStore.json';
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
+// IPFS configuration
+const ipfs = ipfsAPI({ host: 'localhost', port: '5001', protocol: 'http' });
+
+// EcommerceStore is our usable abstraction, which we'll use through the code below.
 const EcommerceStore = contract(ecommerce_store_artifacts);
+
+let reader;
 
 window.App = {
   start: function() {
@@ -17,6 +23,8 @@ window.App = {
 
     // Bootstrap the MetaCoin abstraction for Use.
     EcommerceStore.setProvider(web3.currentProvider);
+
+    // Check render for index and product detail page
     if ($('#product-details').length > 0) {
       const productId = new URLSearchParams(window.location.search).get('id');
       renderProductDetail(productId);
@@ -24,6 +32,14 @@ window.App = {
       renderStore();
     }
 
+    // Handler for image picker changing
+    $('#product-image').change(function(event) {
+      const file = event.target.files[0];
+      reader = new window.FileReader();
+      reader.readAsArrayBuffer(file);
+    });
+
+    // Handler for adding new product
     $('#add-item-to-store').submit(function(event) {
       const req = $('#add-item-to-store').serialize();
       const params = JSON.parse('{"' + req.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
@@ -35,6 +51,7 @@ window.App = {
       event.preventDefault();
     });
 
+    // Handler for buying product
     $('#buy-now').submit(function(event) {
       event.preventDefault();
       $('#msg').hide();
@@ -60,9 +77,14 @@ function renderProductDetail(productId) {
   return EcommerceStore.deployed().then(function(instance) {
     return instance.getProduct.call(productId).then(function(product) {
       $('#product-name').html(product[1]);
+      $('#product-image').html('<img width="100" src="http://localhost:9001/ipfs/' + product[3] + '" />');
       $('#product-price').html(displayPrice(product[6]));
       $('#product-id').val(product[0]);
       $('#buy-now-price').val(product[6]);
+      ipfs.cat(product[4]).then(function (file) {
+        const content = file.toString();
+        $('#product-desc').append('<div>' + content + '</div>');
+      })
     });
   });
 }
@@ -70,16 +92,20 @@ function renderProductDetail(productId) {
 
 /* New product page's functions */
 // Save product to blockchain
-function saveProduct(params) {
-  return EcommerceStore.deployed().then(function(instance) {
-    return instance.addProductToStore(
-      params['product-name'], params['product-category'], 'imageLink', 'descLink',
-      Date.parse(params['product-start-time']) / 1000,
-      web3.toWei(params['product-price'], 'ether'),
-      params['product-condition'],
-      { from: web3.eth.accounts[0], gas: 4700000 },
-    );
-  }).then(function(result) {
+async function saveProduct(params) {
+  // Upload image
+  const imageHash = await saveImageOnIpfs(reader);
+  const descHash = await saveTextBlobOnIpfs(params['product-description']);
+
+  const instance = await EcommerceStore.deployed();
+  return instance.addProductToStore(
+    params['product-name'], params['product-category'],
+    imageHash, descHash,
+    Date.parse(params['product-start-time']) / 1000,
+    web3.toWei(params['product-price'], 'ether'),
+    params['product-condition'],
+    { from: web3.eth.accounts[0], gas: 4700000 },
+  ).then(function(result) {
     window.alert('You have added new product successfully');
   });
 }
@@ -104,6 +130,7 @@ function renderProduct(instance, index) {
   return instance.getProduct.call(index).then(function(product) {
     const node = $('<div />');
     node.addClass('col-sm-3 text-center col-margin-bottom-1 product');
+    node.append('<img src="http://localhost:9001/ipfs/' + product[3] + '" />');
     node.append('<div class="title">' + product[1] + '</div>');
     node.append('<div> Price: ' + displayPrice(product[6]) + '</div>');
     node.append('<a href="product.html?id=' + product[0] + '">Details</a>');
@@ -115,6 +142,32 @@ function renderProduct(instance, index) {
   });
 }
 /* End index page's functions */
+
+function saveTextBlobOnIpfs(blob) {
+  return new Promise(function(resolve, reject) {
+    const buffer = Buffer.from(blob, 'utf-8');
+    ipfs.add(buffer).then(function(response) {
+      console.log(response);
+      resolve(response[0].hash);
+    }).catch(function(err) {
+      console.error(err);
+      reject(err);
+    });
+  });
+}
+
+function saveImageOnIpfs(reader) {
+  return new Promise(function(resolve, reject) {
+    const buffer = Buffer.from(reader.result);
+    ipfs.add(buffer).then(function(response) {
+      console.log(response);
+      resolve(response[0].hash);
+    }).catch(function(err) {
+      console.error(err);
+      reject(err);
+    });
+  });
+}
 
 function displayPrice(price) {
   return '&Xi;' + web3.fromWei(price, 'ether');
