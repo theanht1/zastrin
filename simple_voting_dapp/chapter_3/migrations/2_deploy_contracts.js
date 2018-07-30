7fe6ea045d27a7eec95d7b64b7df5e3e353923a1
Anh Mai <theanht1k20@gmail.com>
2018-07-30 09:24:34 +0700

    Voting app with token

--- simple_voting_dapp/chapter_3/migrations/2_deploy_contracts.js
+++ simple_voting_dapp/chapter_3/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Voting);
+  deployer.deploy(Voting, 10000, web3.toWei(0.01, 'ether'), ['Spiderman','Ironman','Hulk', 'Thor']);
 };
